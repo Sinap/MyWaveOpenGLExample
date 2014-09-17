3619eff257f1fe504ed206a36ef67a4be30f9d32
Aaron Frase <afrase91@gmail.com>
2014-09-17 07:56:51 -0400

    removed duplicate include

--- src/call_backs.c
+++ src/call_backs.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <OpenGL/glu.h>
 #include "call_backs.h"
 #include "wave.h"
 
@@ -16,7 +15,7 @@ void cursor_position_callback(GLFWwindow* window, double x, double y)
         alpha += (GLfloat) (x - cursorX) / 10.f;
         beta += (GLfloat) (y - cursorY) / 10.f;
     }
-    
+
     // save cursors position for use as delta
     cursorX = (int) x;
     cursorY = (int) y;
@@ -33,7 +32,7 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 {
     if (button != GLFW_MOUSE_BUTTON_LEFT)
         return;
-    
+
     // hide cursor when pressing left mouse button down
     if (action == GLFW_PRESS) {
         glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
@@ -48,7 +47,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 {
     if (action != GLFW_PRESS)
         return;
-    
+
     switch (key) {
         // exit on escape
         case GLFW_KEY_ESCAPE:
@@ -117,19 +116,19 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     float ratio = 1.f;
-    
+
     if (height > 0) {
         ratio = (float) width / (float) height;
     }
-    
+
     // setup viewport
     glViewport(0, 0, width, height);
-    
+
     // change to the projection matrix and set our viewing volume
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(60.0, ratio, 1.0, 1024.0);
-    
+
 }
 
 void error_callback(int error, const char* description)
